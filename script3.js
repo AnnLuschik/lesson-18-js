0e7ab6ff18870aaf88ccaece912cc6b4e7ef320d
HP <efafafaef>
2020-08-03 13:40:44 +0300

    feat: add timerSecond and random color

--- script3.js
+++ script3.js
@@ -1,9 +1,16 @@
 let listOfLi = document.querySelectorAll('li');
 
-let timerId = setInterval(() => {
+let timerFirst = setInterval(() => {
+    let randomLi = Math.floor(Math.random() * listOfLi.length) + 0;
+    listOfLi[randomLi].style.color = `#${Math.random().toString(16).slice(2,8)}`;
+ }, 1000);
+
+ clearInterval(timerFirst);
+
+ let timerSecond = setInterval(() => {
     let randomLi = Math.floor(Math.random() * listOfLi.length) + 0;
     for(let li of listOfLi) {
-        if(li.style.color === 'red') li.style.color = 'black';
-    } 
-    listOfLi[randomLi].style.color = 'red';
+        if (li.style.color != '#000000') li.style.color = '#000000';
+    }
+    listOfLi[randomLi].style.color = `#${Math.random().toString(16).slice(2,8)}`;
  }, 1000);
